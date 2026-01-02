ea0289b61c660047ede0cdc15afee1a18f31f0a7
k.munyede <k.munyede@gmail.com>
2026-01-02 08:40:15 +0000

    Changes before Firebase Studio auto-run

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
@@ -15,6 +16,7 @@ void main(List<String> args) async {
   debugPrintLayouts = true;
 
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
   bool isPrimaryInstance = true;
   if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
